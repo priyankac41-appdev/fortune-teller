8dfcb99e95ceca4ec381b58d798df8b9aa8dd523
priyankac41 <pchandr1@chicagobooth.edu>
2023-01-25 20:41:01 +0000

    debug start

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -9,5 +9,17 @@ class NumbersController < ApplicationController
     end
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
+  end
+
+  def losers
+    @zebra = Array.new
+
+    5.times do
+      giraffe = rand(1...100)
+      
+      @zebra.push(giraffe)
+    end
+
+    render({ :template => "lottery_stuff/boo.html.erb"})
   end 
 end
