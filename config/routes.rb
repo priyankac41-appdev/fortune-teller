e554f5fdf1334e04ad2c402f5aa9126f37658494
priyankac41 <pchandr1@chicagobooth.edu>
2023-03-09 23:37:19 +0000

    mostly done

--- config/routes.rb
+++ config/routes.rb
@@ -21,14 +21,14 @@ Rails.application.routes.draw do
   get("/zodiacs/sagittarius", { :controller => "fires", :action => "archer" })
   
   get("/zodiacs/taurus", { :controller=> "earth", :action=> "bull" })
-  # get("/zodiacs/virgo", { :controller => "earth", :action => "maiden" })
-  # get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
+  get("/zodiacs/virgo", { :controller => "earth", :action => "maiden" })
+  get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
   
-  # get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
-  # get(" /zodiacs/libra", { :controller => "air", :action => "scales" })
-  # get ("/zodiacs/aquarius", { :controller => "air", :action => "waterbearer" })
+  get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
+  get("/zodiacs/libra", { :controller => "air", :action => "scales" })
+  get("/zodiacs/aquarius", { :controller => "air", :action => "waterbearer" })
   
-  # get("/zodiacs/cancer " { :controller => "earth" :action => "crab" })
+  get("/zodiacs/cancer " { :controller => "earth" :action => "crab" })
   # get("/zodiac/ scorpio", { :controller => "Water", :action => "scorpion" })
   # get("/zodiacs/pisces", { :controller => "water", :action => "fish " })
 
